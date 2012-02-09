d790bbd1e31ac4b6edf2396948ce7f6cd4efbf71
Chris Edwards <chris@leopipadesigns.co.uk>
2012-02-09 14:40:51 +0000

    Update lib/breadcrumbs_on_rails/breadcrumbs.rb

--- lib/breadcrumbs_on_rails/breadcrumbs.rb
+++ lib/breadcrumbs_on_rails/breadcrumbs.rb
@@ -83,13 +83,13 @@ module BreadcrumbsOnRails
       def render
         @elements.collect do |element|
           render_element(element)
-        end.join(@options[:separator] || " &raquo; ")
+        end
       end
 
       def render_element(element)
         content = @context.link_to_unless_current(compute_name(element), compute_path(element), element.options)
         if @options[:tag]
-          @context.content_tag(@options[:tag], content)
+          @context.content_tag(@options[:tag], content, content_tag(@options[:separator] || " &raquo; "))
         else
           content
         end
