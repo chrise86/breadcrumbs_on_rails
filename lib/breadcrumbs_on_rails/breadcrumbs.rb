4c12c936aff2d23c48e8bbe5379415141b68fe6c
Chris Edwards <chris@leopipadesigns.co.uk>
2012-02-09 14:48:03 +0000

    Update lib/breadcrumbs_on_rails/breadcrumbs.rb

--- lib/breadcrumbs_on_rails/breadcrumbs.rb
+++ lib/breadcrumbs_on_rails/breadcrumbs.rb
@@ -90,7 +90,6 @@ module BreadcrumbsOnRails
         content = @context.link_to_unless_current(compute_name(element), compute_path(element), element.options)
         if @options[:tag]
           @context.content_tag(@options[:tag], content)
-          end
         else
           content
         end
